8090d43f251500e0db818efec9d5da511744cd1c
AlMirai <live.almirai@outlook.com>
2022-12-12 22:49:14 +0800

    v0.1.0

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="al-utils",
-    version="0.0.1",
+    name="al-utils-almirai",
+    version="0.1.0",
     author="AlMirai",
     author_email="live.almirai@outlook.com",
     description="Python common basic utilities for almirai projects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/AlMiraiABC/py-common-utils.git",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/AlMiraiABC/py-common-utils/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
